15f897cc71eaaf3b56f78e4d666dd73519cd9f38
Muse <musemy8484@gmail.com>
2026-06-26 10:23:09 +0300

    add svg icons

---
+++ src/components/icons/minor.tsx
@@ -0,0 +1,34 @@
+const Minor = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+      className={className}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M452.266667 955.733333l-384-384c-34.133333-34.133333-34.133333-87.466667 0-121.6l384-384c34.133333-34.133333 87.466667-34.133333 121.6 0l384 384c34.133333 34.133333 34.133333 87.466667 0 121.6l-384 384c-34.133333 34.133333-89.6 34.133333-121.6 0z"
+          fill="#4CAF50"
+        ></path>
+        <path
+          d="M512 712.533333L362.666667 533.333333h298.666666z"
+          fill="#4CAF50"
+        ></path>
+        <path
+          d="M469.333333 315.733333h85.333334v262.4h-85.333334z"
+          fill="#4CAF50"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+export default Minor;
---
+++ src/components/icons/voltage.tsx
@@ -0,0 +1,26 @@
+const Voltage = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      fill="#ff0000"
+      viewBox="0 0 24.00 24.00"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#ff0000"
+      stroke-width="0.00024000000000000003"
+      className={className}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path d="M15.9999 7.40001C15.0999 7.40001 14.2999 7.20001 13.4999 6.90001L9.5999 5.00001C8.1999 4.30001 5.8999 4.70001 4.8999 5.80001L3.7999 7.00001L2.3999 5.60001L3.4999 4.40001C5.0999 2.60001 8.2999 2.00001 10.4999 3.10001L14.4999 5.00001C15.8999 5.70001 18.1999 5.30001 19.1999 4.20001L20.1999 3.00001L21.6999 4.30001L20.5999 5.50001C19.4999 6.70001 17.7999 7.40001 15.9999 7.40001Z"></path>{" "}
+        <path d="M14.5999 10.9H16.9999L13.1999 22H10.5999L6.8999 10.9H9.2999L11.9999 20L14.5999 10.9Z"></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export default Voltage;
