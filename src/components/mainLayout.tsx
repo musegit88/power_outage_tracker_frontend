796c9605f87f1a971ae9810e30e6f43b716738e8
Muse <musemy8484@gmail.com>
2026-06-28 22:44:43 +0300

    fix: add map loading state provider

--- src/components/mainLayout.tsx
+++ src/components/mainLayout.tsx
@@ -4,20 +4,23 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/app-sidebar";
 import Navbar from "@/components/navbar";
 import { LocationProvider } from "@/providers/locationProvider";
+import { MapLoadingStateProvider } from "@/providers/mapLoadingStateProvider";
 
 const MainLayout = () => {
   return (
     <div className="h-screen">
       <LocationProvider>
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <Navbar />
-            <main className="w-full h-full">
-              <Outlet />
-            </main>
-          </SidebarInset>
-        </SidebarProvider>
+        <MapLoadingStateProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <Navbar />
+              <main className="w-full h-full">
+                <Outlet />
+              </main>
+            </SidebarInset>
+          </SidebarProvider>
+        </MapLoadingStateProvider>
       </LocationProvider>
     </div>
   );
