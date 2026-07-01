57c41902969672265b575f12924aad96229f853c
Muse <musemy8484@gmail.com>
2026-07-01 15:46:37 +0300

    feat(ui): add outage marker on the live map

---
+++ src/components/marker.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import { cn } from "@/lib/utils";
+import type { Outage } from "@/types";
+
+const Marker = ({
+  data,
+  map,
+  isActive,
+  onClick,
+}: {
+  data: Outage;
+  map: mapboxgl.Map;
+  isActive: boolean;
+  onClick: (outage: Outage) => void;
+}) => {
+  const [markerNode] = useState(() => document.createElement("div"));
+
+  useEffect(() => {
+    if (!map) return;
+
+    const marker = new mapboxgl.Marker({
+      element: markerNode,
+    })
+      .setLngLat([Number(data.longitude), Number(data.latitude)])
+      .addTo(map);
+
+    marker.getElement().addEventListener("click", () => {
+      // Propagation handled in React onClick
+    });
+
+    return () => {
+      marker.remove();
+    };
+  }, [data.latitude, data.longitude, map, markerNode]);
+
+  return createPortal(
+    <div
+      className={cn(
+        "relative w-4 h-4 bg-amber-500 rounded-full cursor-pointer",
+        data.status === "ACTIVE"
+          ? "bg-red-500"
+          : data.status === "RESOLVED"
+            ? "bg-green-500"
+            : "bg-yellow-500",
+        isActive && data.status === "RESOLVED" && "bg-green-500",
+        isActive && data.status === "ACTIVE" && "bg-red-500",
+        isActive && data.status === "INVESTIGATING" && "bg-yellow-500",
+      )}
+      onClick={(e) => {
+        // stop propagation to prevent the map's click event from firing
+        e.stopPropagation();
+        onClick(data);
+      }}
+    >
+      <div
+        className={cn(
+          isActive &&
+            "absolute bg-red-500/20 w-8 h-8 rounded-full -top-2 -left-2 -z-10 animate-ping animation-duration-1500",
+          data.status === "RESOLVED" && "bg-green-500/20",
+          data.status === "ACTIVE" && "bg-red-500/20",
+          data.status === "INVESTIGATING" && "bg-yellow-500/20",
+        )}
+      ></div>
+    </div>,
+    markerNode,
+  );
+};
+
+export default Marker;
