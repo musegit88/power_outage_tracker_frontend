d0951c920ac6b55b3239b07faeba76ec00860399
Muse <musemy8484@gmail.com>
2026-07-01 15:57:01 +0300

    feat(router): validate live-map search parameters and pass them to map container

--- src/components/liveMap.tsx
+++ src/components/liveMap.tsx
@@ -1,9 +1,13 @@
+import { useSearch } from "@tanstack/react-router";
 import Map from "./maps/map";
 
 const LiveMap = () => {
+  const { status, limit, offset } = useSearch({
+    from: "/_mainLayout/live-map",
+  });
   return (
     <div className="w-full h-full">
-      <Map />
+      <Map limit={limit} offset={offset} status={status} />
     </div>
   );
 };
--- src/routes/_mainLayout/live-map.tsx
+++ src/routes/_mainLayout/live-map.tsx
@@ -1,6 +1,18 @@
+import z from "zod";
+
 import LiveMap from "@/components/liveMap";
 import { createFileRoute } from "@tanstack/react-router";
 
+const liveMapSearchSchema = z.object({
+  limit: z.number().optional().default(50),
+  offset: z.number().optional().default(0),
+  status: z
+    .enum(["ALL", "ACTIVE", "RESOLVED", "INVESTIGATING"])
+    .optional()
+    .default("ALL"),
+});
+
 export const Route = createFileRoute("/_mainLayout/live-map")({
+  validateSearch: liveMapSearchSchema,
   component: LiveMap,
 });
