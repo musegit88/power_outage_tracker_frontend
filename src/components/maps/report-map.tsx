90724a07b5872632fac32207ded36a46b8649393
Muse <musemy8484@gmail.com>
2026-09-19 14:28:25 +0300

    fix(map): add custom draggable marker with popup for user location

--- src/components/maps/report-map.tsx
+++ src/components/maps/report-map.tsx
@@ -71,9 +71,20 @@ const ReportMap = ({
       [38.935590926262506, 9.089991658552165],
     ]);
 
+    // custom marker element
+    const element = document.createElement("div");
+    element.style.width = "32px";
+    element.style.height = "32px";
+    element.style.color = "#4268ff";
+    element.innerHTML = `<svg title="drag the icon to change location" viewBox="-4 0 32 32" xmlns="http://www.w3.org/2000/svg" width="32" height="32"><path fill="currentColor" transform="translate(-106, -413)" d="M118,422 C116.343,422 115,423.343 115,425 C115,426.657 116.343,428 118,428 C119.657,428 121,426.657 121,425 C121,423.343 119.657,422 118,422 L118,422 Z M118,430 C115.239,430 113,427.762 113,425 C113,422.238 115.239,420 118,420 C120.761,420 123,422.238 123,425 C123,427.762 120.761,430 118,430 L118,430 Z M118,413 C111.373,413 106,418.373 106,425 C106,430.018 116.005,445.011 118,445 C119.964,445.011 130,429.95 130,425 C130,418.373 124.627,413 118,413 L118,413 Z"/></svg>`;
+    const locationPopup = new mapboxgl.Popup({
+      offset: 25,
+      closeButton: false,
+    }).setText(`Your location`);
     // Adding draggable marker to the user's location
-    const marker = new mapboxgl.Marker({ draggable: true })
+    const marker = new mapboxgl.Marker({ draggable: true, element })
       .setLngLat([initialLng, initialLat])
+      .setPopup(locationPopup)
       .addTo(map);
 
     markerRef.current = marker;
